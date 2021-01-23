23c6452991601d64219da03df27824ea4531b1ef
Renzo <renzo.roca.m@gmail.com>
2021-01-23 13:53:19 +0100

    Iteration 1 done

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,36 +3,55 @@ const httpError = require('http-errors');
 const User = require('../models/user.model');
 
 module.exports.register = (req, res, next) => {
-  res.render('users/register');
+    res.render('users/register');
 };
 
 module.exports.doRegister = (req, res, next) => {
 
-  function renderWithErrors(errors) {
-    res.status(400).render('users/register', {
-      user: req.body,
-      errors: errors,
-    });
-  }
+    function renderWithErrors(errors) {
+        res.status(400).render('users/register', {
+            user: req.body,
+            errors: errors,
+        });
+    }
 
-  // Iteration 1: register and validate user
+    // Iteration 1: register and validate user
+    User.findOne({ email: req.body.email })
+        .then((user) => {
+            if (user) {
+                renderWithErrors({ email: 'Email is already registered' });
+            } else {
+                return User.create(req.body).then((user) => {
+                    res.render('users/login');
+                });
+            }
+
+        })
+        .catch((error) => {
+            if (error instanceof mongoose.Error.ValidationError) {
+                renderWithErrors(error.errors);
+            } else {
+                next(error);
+            }
+        });
+
 };
 
 module.exports.login = (req, res, next) => {
-  res.render('users/login');
+    res.render('users/login');
 };
 
 module.exports.doLogin = (req, res, next) => {
-  // Iteration 2: login user
-  // Iteration 4: clean this method and login the user with passport
+    // Iteration 2: login user
+    // Iteration 4: clean this method and login the user with passport
 };
 
 module.exports.logout = (req, res, next) => {
-  // Iteration 2: logout
+    // Iteration 2: logout
 };
 
 module.exports.list = (req, res, next) => {
-  User.find()
-    .then(users => res.render('users/list', { users }))
-    .catch(next)
+    User.find()
+        .then(users => res.render('users/list', { users }))
+        .catch(next)
 }
